f9c3753383b86e87bb4219f7dcc0337fe88614a0
asrani <asrani.27@gmail.com>
2021-11-01 13:31:13 +0800

    F

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>JUDUL</title>
+  <title>SIMAKJAR</title>
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="/theme/plugins/fontawesome-free/css/all.min.css">
@@ -24,7 +24,7 @@
     <div class="container">
       <a href="/" class="navbar-brand">
         <img src="/theme/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">PERDAGIN</span>
+        <span class="brand-text font-weight-light">SIMAKJAR</span>
       </a>
 
       <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
